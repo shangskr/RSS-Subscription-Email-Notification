f58b8bfecbf917c6bbee8e951e437efcf03def46
安小歪 <119773750+shangskr@users.noreply.github.com>
2024-07-02 16:35:47 +0800

    通过ThreadPoolExecutor提升了处理性能

--- check_rss.py
+++ check_rss.py
@@ -48,9 +48,10 @@ def fetch_feed_with_requests(rss_url):
         print(f"访问 {rss_url} 出错: {e}")
         return None
 
-def check_and_notify_single(rss_url):
-    rss_url = rss_url.strip()
-    
+def process_rss(rss_url):
+    updated = False
+    message_content = ""
+
     if rss_url in special_rss_urls:
         feed = fetch_feed_with_requests(rss_url)
     else:
@@ -79,7 +80,7 @@ def check_and_notify_single(rss_url):
     new_entries = [entry for entry in feed.entries if entry.get('published_parsed') and time.mktime(entry.published_parsed) > last_check_time]
 
     if new_entries:
-        message_content = ""
+        updated = True
         for entry in new_entries:
             entry_title = entry.get('title', 'No Title')
             entry_link = entry.get('link', 'No Link')
@@ -91,29 +92,18 @@ def check_and_notify_single(rss_url):
         latest_time = max(time.mktime(entry.published_parsed) for entry in new_entries)
         with open(last_check_file, 'w') as f:
             f.write(str(latest_time))
-
-        return feed_title, message_content
     else:
         print(f"{feed_title} 没有新文章")
-        return None, None
-
-def check_and_notify():
-    updated = False
-    message_content = ""
-
-    with ThreadPoolExecutor(max_workers=5) as executor:  # 根据需要调整最大工作线程数
-        futures = [executor.submit(check_and_notify_single, rss_url) for rss_url in rss_list]
-        
-        for future in as_completed(futures):
-            feed_title, feed_content = future.result()
-            if feed_content:
-                updated = True
-                message_content += feed_content
 
     if updated:
         send_email("RSS更新提醒", message_content)
-    else:
-        print("没有RSS源更新")
+
+def check_and_notify():
+    with ThreadPoolExecutor(max_workers=5) as executor:  # 根据需要调整最大工作线程数
+        futures = [executor.submit(process_rss, rss_url.strip()) for rss_url in rss_list]
+
+        for future in as_completed(futures):
+            future.result()
 
 def send_email(subject, message):
     msg = MIMEMultipart()
